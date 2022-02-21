af876998ec4d946573b4cd883902fcc992623527
Matheus Santos <matheusmatmac@hotmail.com>
2022-02-21 14:51:03 -0300

    feat: Refactor codem added check words and adjust return score

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 from timeit import timeit
 from Levenshtein import distance as lev
+from array import array
 
-setForbiddenNames = set(map(str.casefold, [
-    'Corona', 'Clorivaldo', 'Pfizernaldo', 'Hulk', 'Naruto', 'Goku', 'Cloroquina', 'Hitler']))
+setForbiddenNames = ['adolf hitler', 'barack obama']
 
 
 def calculateLevenshteinDistance(name):
-    for n in name.split():
-        for f in setForbiddenNames:
-            if(lev(n.casefold(), f.casefold()) <= 3):
-                return False
+    result = []
+    name = checkWords(name)
 
-    return True
+    for f in setForbiddenNames:
+        result.append(lev(name, f))
+
+    return result
 
 
-name = 'Pedro da H1t7er Silva'
+def checkWords(name):
+    if '1' in name:
+        name = name.replace('1', 'i')
+    if '3' in name:
+        name = name.replace('3', 'e')
+    if '4' in name:
+        name = name.replace('4', 'a')
+    if '5' in name:
+        name = name.replace('5', 's')
+    if '7' in name:
+        name = name.replace('7', 't')
+    if '8' in name:
+        name = name.replace('8', 'b')
+    if '0' in name:
+        name = name.replace('0', 'o')
+
+    return name
+
+
+name = 'pedro da h1t7er silva'
 
 params = {'number': 10000, 'globals': globals()}
 
