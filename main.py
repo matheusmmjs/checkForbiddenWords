ad36c61bb9a92265a9cb4550874e3c8d576b8a81
Matheus Santos <matheusmatmac@hotmail.com>
2022-02-22 11:00:45 -0300

    refactor: Adjust code

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 from timeit import timeit
 from Levenshtein import distance as lev
-from array import array
 
-setForbiddenNames = ['adolf hitler', 'barack obama']
+set_forbidden_names = ["adolf hitler", "barack obama"]
 
 
-def calculateLevenshteinDistance(name):
+def calculate_levenshtein_distance(name):
     score = 0
-    minScore = 99999
-    name = checkWords(name)
+    min_score = 99999
+    name = check_words(name)
 
-    for f in setForbiddenNames:
+    for f in set_forbidden_names:
         score = lev(name, f)
 
-        if(score < minScore):
-            minScore = score
+        if(score < min_score):
+            min_score = score
 
-    return minScore
+    return min_score
 
 
-def checkWords(name):
-    if '1' in name:
-        name = name.replace('1', 'i')
-    if '3' in name:
-        name = name.replace('3', 'e')
-    if '4' in name:
-        name = name.replace('4', 'a')
-    if '5' in name:
-        name = name.replace('5', 's')
-    if '7' in name:
-        name = name.replace('7', 't')
-    if '8' in name:
-        name = name.replace('8', 'b')
-    if '0' in name:
-        name = name.replace('0', 'o')
+def check_words(name):
+    if "1" in name:
+        name = name.replace("1", "i")
+    if "3" in name:
+        name = name.replace("3", "e")
+    if "4" in name:
+        name = name.replace("4", "a")
+    if "5" in name:
+        name = name.replace("5", "s")
+    if "7" in name:
+        name = name.replace("7", "t")
+    if "8" in name:
+        name = name.replace("8", "b")
+    if "0" in name:
+        name = name.replace("0", "o")
 
     return name
 
 
-name = 'pedro henrique da silva'
+name = "pedro henrique da silva"
 
-params = {'number': 10000, 'globals': globals()}
+params = {"number": 10000, "globals": globals()}
 
-print(timeit('calculateLevenshteinDistance(name)', **params))
+print(timeit("calculate_levenshtein_distance(name)", **params))
 
-print((calculateLevenshteinDistance(name)))
+print((calculate_levenshtein_distance(name)))
